6f051fe2ec792c94fdd5b1909c5357c7ac4f2284
Sally Lee <sallylrs@sunway.com.my>
2025-07-14 07:50:11 +0800

    comment and refactor code

--- controllers/main_window_controller.py
+++ controllers/main_window_controller.py
@@ -7,10 +7,13 @@ class MainWindowController(QObject):
     def __init__(self):
         super().__init__()
 
+        # Initialize the main window view
         self.main_window = MainWindow()
+
+        # Store a list of visited page for page nagivation during undo (back)
         self.visited_content_pages = []
 
-        # Connect buttons
+        # Connect the page_selected signals to the switch_page method for switching pages
         self.main_window.sidebar.page_selected.connect(self.switch_page)
         self.main_window.log_in_page.page_selected.connect(self.switch_page)
         self.main_window.sign_in_page.page_selected.connect(self.switch_page)
@@ -27,6 +30,7 @@ class MainWindowController(QObject):
         self.main_window.physical_exercise_selection_page.page_selected.connect(self.switch_page)
         self.main_window.rest_page.page_selected.connect(self.switch_page)
 
+        # Connect the is_notification signal to the bring_window_to_front method for displaying the window on top of all other running apps
         self.main_window.focus_zone_page.view.attention_detector.is_notification_start.connect(self.bring_window_to_front)
         self.main_window.rest_page.is_notification.connect(self.bring_window_to_front)
         self.main_window.physical_exercise_1_page.is_notification.connect(self.bring_window_to_front)
@@ -34,44 +38,72 @@ class MainWindowController(QObject):
         self.main_window.physical_exercise_3_page.is_notification.connect(self.bring_window_to_front)
 
     def run(self):
+        """Show the main window."""
+
         self.main_window.showMaximized()
 
     def switch_app_page(self, app_page_name):
+        """Switch the app-level pages."""
+
         app_page = self.main_window.app_pages.get(app_page_name)
         if app_page:
             match (app_page_name):
                 case "main":
+                    # Display the main content
+                    # Get the current user's information through the previously created login session singleton
                     login_session = LoginSession()
                     username = login_session.get_username()
+                    # Reload home page with the latest user statistics
                     self.main_window.home_page.load_home_page()
+                    # Set the username to show the logged in user session
                     self.main_window.sidebar.view.user_profile_btn.setText(username)
                 case "log_in":
+                    # Display the login page
+                    # Clear the user's information in the previously created login session singleton
                     login_session = LoginSession()
                     login_session.clear_user()
+                    # Reset the visited content pages records
                     self.visited_content_pages = []
                 case "sign_in":
+                    # Display the signin page
+                    ## Clear the user's information in the previously created login session singleton
                     login_session = LoginSession()
                     login_session.clear_user()
+                    # Reset the visited content pages records
                     self.visited_content_pages = []
                     
+            # Switch to the selected app page in the stacked widget
             self.main_window.app_pages_stack.setCurrentWidget(app_page)
 
     def switch_content_page(self, content_page_name):
+        """Switch the content-level pages."""
+
         content_page = self.main_window.content_pages.get(content_page_name)
         if content_page:
+            # Save page navigation history
             self.visited_content_pages.append(content_page_name)
+            
             match (content_page_name):
                 case "home":
+                    # Reload the home page for the latest user statistics before displaying
                     self.main_window.home_page.load_home_page()
                 case "insights":
+                    # Reload the insights page for the latest user statistics before displaying
                     self.main_window.insights_page.load_insights()
                 case "leaderboard":
+                    # Reload the leaderboard page for the latest user statistics before displaying
                     self.main_window.leaderboard_page.load_leaderboard()
+            
+            # Switch to the selected content page in the stacked widget
             self.main_window.content_pages_stack.setCurrentWidget(content_page)
     
     def switch_page(self, page_name):
+        """Switch between app-level and content-level pages and handle back navigation"""
+
         if page_name == "back":
+            # Remove the current page from history
             self.visited_content_pages.pop()
+            # Switch to the previous one
             self.switch_content_page(self.visited_content_pages[-1])
         elif self.main_window.app_pages.get(page_name):
             self.switch_app_page(page_name)
@@ -79,6 +111,8 @@ class MainWindowController(QObject):
             self.switch_content_page(page_name)
 
     def bring_window_to_front(self):
+        """Brings the main window to the front of all other running apps and gives it focus."""
+
         self.main_window.showMaximized()  
         self.main_window.raise_()      # Bring window to front
         self.main_window.activateWindow()  # Give focus
--- views/main_window.py
+++ views/main_window.py
@@ -1,5 +1,3 @@
-
-
 from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QStackedWidget
 
 from controllers.activity_page_controller import ActivityPageController
@@ -12,21 +10,24 @@ from controllers.sidebar_widget_controller import SidebarController
 from controllers.log_in_page_controller import LogInPageController
 from controllers.sign_in_page_controller import SignInPageController
 from controllers.focus_zone_page_controller import FocusZonePageController
-from controllers.insights_page_controller import InsightsPage, InsightsPageController
+from controllers.insights_page_controller import InsightsPageController
 from models.data import ActivityPageConfig
-from views.activity_page import ActivityPage
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()  
         
+        # Stack for switching between main app pages (login, sign in, main content)
         self.app_pages_stack = QStackedWidget()
         
-        self.content_page_widget = QWidget()
-        self.content_page_widget_layout = QHBoxLayout(self.content_page_widget)
+        # Widget and layout for the main content area (sidebar + content pages stack)
+        self.content_area_widget = QWidget()
+        self.content_area_widget_layout = QHBoxLayout(self.content_area_widget)
+        
+        # Stack for switching between content pages (home, focus zone, insights, etc.)
         self.content_pages_stack = QStackedWidget()
 
-        # Initialize controllers
+        # Initialize controllers for all pages and sidebar
         self.sidebar = SidebarController()
         self.log_in_page = LogInPageController()
         self.sign_in_page = SignInPageController()
@@ -37,32 +38,40 @@ class MainWindow(QMainWindow):
         self.rest_page = RestPageController()
         self.leaderboard_page = LeaderboardPageController()
         
+        # Create configs for mindfulness activities
         mindfulness_activity_1_page_config = ActivityPageConfig(page_title="The Wheel of Awareness", description="Expand your focus by gently guiding your attention through different aspects of your experience.\nThis practice helps you strengthen your awareness by moving attention across your senses, thoughts, and feelings. It cultivates calmness, clarity, and mental flexibility — supporting you in regaining focus when distractions arise.", video_embed_link="https://www.youtube.com/embed/kZrjmPPvE7k?si=4Xhg2hxAkch6i5L5", timer_duration=0, page_name="mindfulness_activity_1")
         mindfulness_activity_2_page_config = ActivityPageConfig(page_title="Leaves on a Stream", description="Let go of thoughts by imagining them floating away on a gentle stream.\nThis exercise invites you to observe your thoughts without judgment. As each thought arises, place it on a leaf and watch it drift away. It promotes mental clarity and emotional distance from distractions, helping you return to a centered, focused state.", video_embed_link="https://www.youtube.com/embed/Ml-yuYraZkA?si=rfxn9jEaBtkWY5gG", timer_duration=0, page_name="mindfulness_activity_2")
         mindfulness_activity_3_page_config = ActivityPageConfig(page_title="Eye of the Hurricane", description="Find calm at the center of life’s storms, just like the stillness at the eye of a hurricane.\nThis guided activity encourages you to notice the chaos around you while anchoring yourself in an inner place of calm. It builds emotional resilience and focus, allowing you to stay grounded even when your mind feels scattered.", video_embed_link="https://www.youtube.com/embed/HPFayxlm_ms?si=Yy7yeFQYAUxBuV6T", timer_duration=0, page_name="mindfulness_activity_3")
+        # Store configs in a list for selection page rendering
         self.mindfulness_activity_page_configs = [mindfulness_activity_1_page_config, mindfulness_activity_2_page_config, mindfulness_activity_3_page_config]
         
+        # Create Controllers for mindfulness activity selection and individual activity pages
         self.mindfulness_activity_selection_page = ActivitySelectionPageController("Mindfulness Activity", self.mindfulness_activity_page_configs)
         self.mindfulness_activity_1_page = ActivityPageController(mindfulness_activity_1_page_config)
         self.mindfulness_activity_2_page = ActivityPageController(mindfulness_activity_2_page_config)
         self.mindfulness_activity_3_page = ActivityPageController(mindfulness_activity_3_page_config)
 
+        # Create configs for physical exercise activities
         physical_exercise_1_page_config = ActivityPageConfig(page_title="High Knee Jog in Place", description="", video_embed_link="https://www.youtube.com/embed/IpnHwUwrVVY?si=lFJ1Ap-0PKZ9RcI0", timer_duration=30, page_name="physical_exercise_1")
         physical_exercise_2_page_config = ActivityPageConfig(page_title="Star Jump", description="", video_embed_link="https://www.youtube.com/embed/VVEO_J1tIXU?si=yI216dgsgMbnaIvG", timer_duration=300, page_name="physical_exercise_2")
         physical_exercise_3_page_config = ActivityPageConfig(page_title="Brisk Walking", description="", video_embed_link="https://www.youtube.com/embed/tVpUCkMLgms?si=x58F0AWgP2YyQDtF", timer_duration=300, page_name="physical_exercise_3")
+        # Store configs in a list for selection page rendering
         self.physical_exercise_page_configs = [physical_exercise_1_page_config, physical_exercise_2_page_config, physical_exercise_3_page_config]
         
+        # Create Controllers for physical exercise selection and individual activity pages
         self.physical_exercise_selection_page = ActivitySelectionPageController("Physical Exercise", self.physical_exercise_page_configs)
         self.physical_exercise_1_page = ActivityPageController(physical_exercise_1_page_config)
         self.physical_exercise_2_page = ActivityPageController(physical_exercise_2_page_config)
         self.physical_exercise_3_page = ActivityPageController(physical_exercise_3_page_config)
 
+        # Mapping app-level page name to their views
         self.app_pages = {
             "log_in": self.log_in_page.view,
             "sign_in": self.sign_in_page.view,
-            "main": self.content_page_widget
+            "main": self.content_area_widget
         }
 
+        # Mapping content-level page name to their views
         self.content_pages = {
             "home": self.home_page.view,
             "focus_zone": self.focus_zone_page.view,
@@ -80,20 +89,23 @@ class MainWindow(QMainWindow):
             "leaderboard": self.leaderboard_page.view
         }
 
+        # Add the views of app-level pages to the app pages stack
         for app_pages in self.app_pages.values():
             self.app_pages_stack.addWidget(app_pages)
 
+        # Add the views of content-level pages to the content pages stack
         for content_pages in self.content_pages.values():
             self.content_pages_stack.addWidget(content_pages)
 
-        self.content_page_widget_layout.addWidget(self.sidebar.view)
-        self.content_page_widget_layout.addWidget(self.content_pages_stack)
+        # Add sidebar and content pages stack to the main content area 
+        self.content_area_widget_layout.addWidget(self.sidebar.view)
+        self.content_area_widget_layout.addWidget(self.content_pages_stack)
 
         self.app_pages_stack.setContentsMargins(0, 0, 0, 0)
-
-        self.content_page_widget_layout.setContentsMargins(0, 0, 0, 0)
-        self.content_page_widget_layout.setSpacing(0)
         self.content_pages_stack.setContentsMargins(0, 0, 0, 0)
+
+        self.content_area_widget_layout.setContentsMargins(0, 0, 0, 0)
+        self.content_area_widget_layout.setSpacing(0)
         
         self.setWindowTitle("Foocus.")
         self.setCentralWidget(self.app_pages_stack)
